8a9050a4b3aaa9daad4b848c50e011ed2be5b34a
Alex KooWeso <koowa360@gmail.com>
2024-04-13 01:15:18 +0300

    added babel rules

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,22 @@ module.exports = {
     mode: 'development',
     output: {
         filename: 'webpack.bundle.js',
+        clean: true,
     },
     modules: {
         rules: [
-            
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
         ],
     },
     plugins: [
