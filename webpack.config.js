cf30811ba42599c727f3d4cfae0dc0712300ef2a
Alex KooWeso <koowa360@gmail.com>
2024-04-13 13:28:04 +0300

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
-    output: {
-        filename: 'webpack.bundle.js',
-        clean: true,
-    },
-    modules: {
-        rules: [
-            {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                  }
-                }
-            },
-            {
-              test: /\.css$/i,
-              use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
+  mode: 'development',
+  output: {
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  modules: {
+    rules: [ //rules
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: "defaults"
+              }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
         ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin(),
-      new MiniCssExtractPlugin(),
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'SCP 1.6',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }), // file name '[name].css' by default
+  ],
 }
