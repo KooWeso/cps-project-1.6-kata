1546cd0e20d45375ea1c1fc162157f67d16ec9f9
Alex KooWeso <koowa360@gmail.com>
2024-04-20 14:00:30 +0300

    path change for easy github conection

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: '[name].[ext]', //describes the name of the assets (css, images, fonts) in the output folder - with their contenthash and file extension 
-    publicPath: '/', //describes the URL of the output folder i dont care
+    publicPath: 'htthttps://github.com/KooWeso/cps-project-1.6-kata/', //describes the URL of the output folder i dont care
     filename: 'scripts/[name].js', //describes the name of the output file - with its contenthash (this one for js)
     clean: true, // when true, removes old dist files automatically with each build
   },
